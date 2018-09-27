410888d973f10920bb74d992c4af91b957fdf68b
liuyongjie <liuyongjiesail@icloud.com>
2018-09-27 17:05:54 +0800

    Initial Commit

---
+++ ImageRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageRecognition
+//
+//  Created by 刘永杰 on 2018/9/27.
+//  Copyright © 2018 刘永杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageRecognition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageRecognition
+//
+//  Created by 刘永杰 on 2018/9/27.
+//  Copyright © 2018 刘永杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
